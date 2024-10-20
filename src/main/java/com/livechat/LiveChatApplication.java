c91874bc382e74d6bbaf6a556b68c3c409b1b1d7
Arthur Lopes <arthurlopr12@gmail.com>
2024-10-20 09:52:54 -0300

    feat: init repo

--- src/main/java/com/livechat/LiveChatApplication.java
+++ src/main/java/com/livechat/LiveChatApplication.java
@@ -1,4 +1,4 @@
-package java.livechat;
+package com.livechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/livechat/LiveChatApplicationTests.java
+++ src/test/java/com/livechat/LiveChatApplicationTests.java
@@ -1,4 +1,4 @@
-package java.livechat;
+package com.livechat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
